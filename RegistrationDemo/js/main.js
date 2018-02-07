790aa5b78d964a69eec7440bebb76b7eaf148587
Shubhangi <sbisaria25@gmail.com>
2018-02-07 16:56:39 +0530

    add validations

--- RegistrationDemo/js/main.js
+++ RegistrationDemo/js/main.js
@@ -5,10 +5,49 @@ var RegisterModel = Backbone.Model.extend({
 		email:null,
 		password:null,
 		cpassword:null
-	}
+	},
+	validate: function(obj){
+		var errors=[],
+		inValid = false;
+		if(obj.firstName==""){
+			errors.push({type: 'firstName', msg:'First name should not be empty'});
+			inValid=true;
+		}
+		  if(obj.lastName==""){
+			errors.push({type: 'lastName', msg:'Last name should not be empty'});	
+			inValid=true;
+		}
+		 if(obj.email=="" ){
+			errors.push({type: 'email', msg:'Email should not be empty'});
+			inValid=true;
+		}
+		 if(obj.password==""){
+			errors.push({type: 'password', msg:'Password should not be empty'});
+			inValid=true;
+		}
+		 if(obj.cpassword == "" || (obj.password!=obj.cpassword)){
+			errors.push({type: 'cpassword', msg:'Confirm password should match password'});
+			inValid=true;
+		}
+		if(inValid){
+			this.trigger('failure',errors);
+		}else{
+			var registerModel = new RegisterModel();
+			registerModel.set({
+			firstName : $("#first-name").val(),
+			lastName : $("#last-name").val(),
+			email : $("#email").val(),
+			password : $("#password").val(),
+			cpassword:$("#cpassword").val()
+		});
+		this.trigger('success',registerModel);
+		}
+    }
 });
 var RegistrationView = Backbone.View.extend({
 	initialize : function(){
+		this.model.on('failure',this.renderError);
+		this.model.on('success',this.renderDetails);
 	},
 	render: function () {
 		var template = _.template($("#registration-template").html());
@@ -18,25 +57,32 @@ var RegistrationView = Backbone.View.extend({
 		"click #js-submit" : "saveDetails"
 	},
 	saveDetails : function(){
-		this.model.set({
+		
+		this.model.validate({
 			firstName : this.$("#first-name").val(),
 			lastName : this.$("#last-name").val(),
 			email : this.$("#email").val(),
 			password : this.$("#password").val(),
 			cpassword:this.$("#cpassword").val()
 		});
-		this.renderDetails();
 	},
-	renderDetails : function(){
-		var self = this;
+	renderError :function(errors){
+		for(var i=0;i<errors.length;i++){
+			var currentDiv= errors[i].type;
+			$("li").find('.'+currentDiv);
+			$('.'+currentDiv+ ' .help-block').text((errors[i]).msg);
+		}
+	},
+	renderDetails : function(newModel){
 		var updatedView = new DetailsView({
 			el : "#container",
-			model : self.model
+			model : newModel
 		}).render();
 	}
 });
 var DetailsView = Backbone.View.extend({
 	initialize : function(){
+
 	},
 	render: function() {
 	  	var template = _.template($("#detail-template").html());
