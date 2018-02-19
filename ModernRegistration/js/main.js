c7a483eb5b5086e906ec001022862c23fbbff0a7
Shubhangi <sbisaria25@gmail.com>
2018-02-19 22:15:06 +0530

    add backing js

--- ModernRegistration/js/main.js
+++ ModernRegistration/js/main.js
@@ -1,51 +1,166 @@
-// FirstView = Backbone.View.extend({
-// 	el : ("#container"),
-// 	initialize: function(){
-// 		this.render();
-// 	},
-// 	render: function(){
-// 		var template = _.template($("#first-template").html(),{});
-// 		this.$el.html(template);
-// 	}
-// });
-// SecondView = Backbone.View.extend({
-// 	el : $("#container"),
-// 	initialize: function(){
-// 		this.render();
-// 	},
-// 	render: function(){
-// 		var template = _.template($("#second-template").html());
-// 		this.$el.html(template);
-// 	}
-// });
-var MyRouter = Backbone.Router.extend({
-	routes:{
-		"foo/bar" : "test",
-		"*action" : "func",
-		// "show/5" : "show"
-		"show/:id" : "show"
-		// "" : "firstPage",
-		// "first" : "firstPage",
-		// "second" : "secondPage"
+var RegisterModel = Backbone.Model.extend({
+	default:{
+		firstName : null,
+		lastName: null,
+		address:null,
+		city:null,
+		pin:null,
+		age:null,
+		gender:null,
+		email:null,
+		mobile1:null,
+		mobile2:null,
+		country:null,
+		nationality:null,
+		password:null,
+		cpassword:null
 	},
-	func : function(action){
-		console.log(action);
-	},
-	test : function(p){
-		console.log(p);
-	},
-	show : function(id){
-		console.log("Show route with correct id : " + id);
-		// model/view/event
-	}
-	// firstPage: function(){
-	// 	// console.log("action performed");
-	// 	new FirstView();
-	// },
-	// secondPage: function(){
-	// 	new SecondView();
-	// }
-	
+	validate: function(obj){
+		var errors = [],
+		inValid = false,
+		emailPattren = /^\w+([-+.'][^\s]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+		if(obj.firstName == ""){
+			errors.push({name: 'firstName', msg:'First name should not be empty'});
+			inValid = true;
+		}
+		if(obj.lastName == ""){
+			errors.push({name: 'lastName', msg:'Last name should not be empty'});
+			inValid = true;
+		}
+		if(obj.address == ""){
+			errors.push({name: 'address', msg:'Address should not be empty'});
+			inValid = true;
+		}
+		if(obj.city == ""){
+			errors.push({name: 'city', msg:'City should not be empty'});
+			inValid = true;
+		}
+		if(obj.pin == ""){
+			errors.push({name: 'pin', msg:'Pin should not be empty'});
+			inValid = true;
+		}
+		if(obj.age == ""){
+			errors.push({name: 'age', msg:'Age should not be empty'});
+			inValid = true;
+		}
+		if(obj.gender == ""){
+			errors.push({name: 'gender', msg:'Gender should not be empty'});
+			inValid = true;
+		}
+		if(!emailPattren.test(obj.email)){
+			errors.push({name: 'email', msg:'Email should be valid'});
+			inValid = true;
+		}
+		if(obj.email == ""){
+			errors.push({name: 'email', msg:'Email should not be empty'});
+			inValid = true;
+		}
+		if(obj.mobile1 == ""){
+			errors.push({name: 'mobile1', msg:'Mobile 1 should not be empty'});
+			inValid = true;
+		}
+		if(obj.mobile2 == ""){
+			errors.push({name: 'mobile2', msg:'Mobile 2 should not be empty'});
+			inValid = true;
+		}
+		if(obj.country == ""){
+			errors.push({name: 'country', msg:'Country should not be empty'});
+			inValid = true;
+		}
+		if(obj.nationality == ""){
+			errors.push({name: 'nationality', msg:'Nationality should not be empty'});
+			inValid = true;
+		}
+		if(obj.password == ""){
+			errors.push({name: 'password', msg:'Password should not be empty'});
+			inValid = true;
+		}
+		if(obj.cpassword == "" && (obj.password != obj.cpassword)){
+			errors.push({name: 'cpassword', msg:'Confirm password should match password'});
+			inValid = true;
+		}
+		if(inValid){
+			this.trigger('failure',errors);
+		}else{
+			this.set({
+				firstName : $("#first-name").val(),
+				lastName : $("#last-name").val(),
+				address : $("#address").val(),
+				city : $("#city").val(),
+				pin : $("#pin").val(),
+				age : $("#age").val(),
+				gender : $("#gender").val(), 
+				email : $("#email").val(),
+				mobile1 : $("#mobile1").val(),
+				mobile2 : $("#mobile2").val(),
+				country : $("#country").val(),
+				nationality : $("#nationality").val(),
+				password : $("#password").val(),
+				cpassword:$("#cpassword").val()
+			});
+			this.trigger('success',this);
+		}
+    }
 });
-var router  = new MyRouter();
-Backbone.history.start();
+var RegistrationView = Backbone.View.extend({
+	initialize : function(){
+		this.model.on('failure',this.renderError);
+		this.model.on('success',this.renderDetails);
+	},
+	render: function () {
+		var template = _.template($("#registration-template").html());
+		this.$el.html(template);
+	},
+	events :{
+		"click #js-submit" : "saveDetails"
+	},
+	saveDetails : function(){
+		this.model.validate({
+			firstName : this.$("#first-name").val(),
+			lastName : this.$("#last-name").val(),
+			address : this.$("#address").val(),
+			city : this.$("#city").val(),
+			pin : this.$("#pin").val(),
+			age : this.$("#age").val(),
+			gender : this.$("#gender").val(),
+			email : this.$("#email").val(),
+			mobile1 : this.$("#mobile1").val(),
+			mobile2 : this.$("#mobile2").val(),
+			country : this.$("#country").val(),
+			nationality : this.$("#nationality").val(),
+			password : this.$("#password").val(),
+			cpassword:this.$("#cpassword").val()
+		});
+	},
+	renderError :function(errors){
+		$('#error').text(' ');
+		errors.forEach(function(element){
+			var currentDiv = element.name;
+			var target =  '.' + currentDiv;
+			$('.' + currentDiv).addClass("has-error");
+			var error = (element).msg;
+			$('.' + currentDiv + ' ' +'.has-error').text("*" + error);
+		});
+	},
+	renderDetails : function(newModel){
+		var updatedView = new DetailsView({
+			el : "#container", 
+			model : newModel
+		}).render();
+	}
+});
+var DetailsView = Backbone.View.extend({
+	initialize : function(){
+
+	},
+	render: function() {
+	  	var template = _.template($("#detail-template").html());
+		this.$el.html(template(this.model.toJSON()));
+	}
+});
+$(document).ready(function(){
+	var defaultView = new RegistrationView({
+		el : "#container",
+		model : new RegisterModel
+	}).render();
+});
